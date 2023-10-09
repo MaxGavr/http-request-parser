f464913c7248262b914eb1590c8b0ded6b9e872a
Max Gavrilyuk <max.gavr.gmi@gmail.com>
2023-10-09 20:48:13 +0300

    Init: boilerplate for library and tests, CMake presets, GitHub workflow

---
+++ source/http_request_parser.cpp
@@ -0,0 +1,8 @@
+#include "http_request_parser/http_request_parser.hpp"
+
+#include <iostream>
+
+void hello_world()
+{
+    std::cout << "Hello, World!\n";
+}
---
+++ source/http_request_parser_test.cpp
@@ -0,0 +1,7 @@
+#include "http_request_parser/http_request_parser.hpp"
+
+int main()
+{
+    hello_world();
+    return 0;
+}
