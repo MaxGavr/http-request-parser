bec384480ad593f0f542cf00f2fbdbf4bb03e797
Max Gavrilyuk <max.gavr.gmi@gmail.com>
2023-10-10 11:32:27 +0300

    Add Catch2

--- source/http_request_parser_test.cpp
+++ source/http_request_parser_test.cpp
@@ -1,6 +1,8 @@
+#include <catch2/catch_test_macros.hpp>
+
 #include "http_request_parser/http_request_parser.hpp"
 
-int main()
+TEST_CASE("Parsing HTTP requests not implemented")
 {
-    return 0;
+    CHECK_THROWS_AS(HttpRequest::Parse(""), std::logic_error);
 }
